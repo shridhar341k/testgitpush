7342a14f929f281a0a001edbcbdc26503c7eb8b8
Shridhar <shridhar341k>
2022-07-19 16:04:06 +0530

    This is my first code push in git

---
+++ mongotest.py
@@ -0,0 +1,16 @@
+import pymongo
+
+client = pymongo.MongoClient("mongodb+srv://shridhar341k:shridhar123@cluster0.hvned.mongodb.net/?retryWrites=true&w=majority")
+db = client.test
+
+print(db)
+
+d={
+    "name":"sudhanshu",
+    "email":"sudhanshi@ineuron.ai",
+    "surname":"kumar"
+}
+db1 = client['mongotest']
+coll=db1['test']
+coll.insert_one(d)
+
